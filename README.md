4c75956005fcfff94d89c734188e458ab0814b46
Sergii <kryvkosergii@gmail.com>
2025-03-13 00:23:08 +0200

    HW3 alg for first task

--- README.md
+++ README.md
@@ -5,7 +5,62 @@ Develop a program to model the flow network for goods logistics from warehouses 
 
 ![Graph](doc/Figure_1.svg)
 
-To draw graph
+### Prerequisites 
+```bash
+poetry install
+poetry shell
+```
+
+### To draw graph
 ```bash
 poetry run python .\app\logistic_draw.py
 ```
+
+### To run alg
+```bash
+poetry run python .\app\logistic.py -matrix
+```
+where 
+-matrix - print capacity matrix. Optional
+
+### Results:
+    <h2>Actual Flow of Goods</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Terminal</th>
+                <th>Shop</th>
+                <th>Actual Flow (units)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr><td>Термінал 1</td><td>Магазин 1</td><td>15</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 2</td><td>10</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 3</td><td>20</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 4</td><td>15</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 5</td><td>10</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 6</td><td>20</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 7</td><td>15</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 8</td><td>15</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 9</td><td>10</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 10</td><td>0</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 11</td><td>0</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 12</td><td>0</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 13</td><td>0</td></tr>
+            <tr><td>Термінал 1</td><td>Магазин 14</td><td>0</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 1</td><td>0</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 2</td><td>0</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 3</td><td>0</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 4</td><td>10</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 5</td><td>10</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 6</td><td>10</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 7</td><td>15</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 8</td><td>15</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 9</td><td>10</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 10</td><td>20</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 11</td><td>10</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 12</td><td>15</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 13</td><td>5</td></tr>
+            <tr><td>Термінал 2</td><td>Магазин 14</td><td>10</td></tr>
+        </tbody>
+    </table>
---
+++ app/logistic.py
@@ -0,0 +1,99 @@
+from logistic_data import labels, edges, sources, sinks
+import argparse
+from collections import deque
+
+parser = argparse.ArgumentParser(description="Googs logistic")
+parser.add_argument("-matrix", action="store_true", help="print capacity matrix")
+args = parser.parse_args()
+
+# The function builds a matrix of capacities for the network flow problem.
+def build_dynamic_capachity_matrix(labels, edges):
+    length = len(labels)
+    matrix = [[0 for i in range(length)] for j in range(length)]
+    for edge in edges:
+        matrix[labels.index(edge[0])][labels.index(edge[1])] = edge[2]
+    return matrix
+
+def label_comprassion(label):
+    return label[0] + label.split()[1]
+
+def print_matrix_with_labels(matrix, labels):
+    cell_width = 5
+    delimiter = f"{'_'*(cell_width*len(labels) + cell_width)}"
+    print(delimiter)
+    print()
+    print(f"{'capacity matrix':^110}")
+    print(delimiter)
+    print(" " * cell_width, end="")
+    for label in labels:
+        print(f"{label_comprassion(label):<{cell_width}}", end="")
+    print()
+    for idx, row in enumerate(matrix):
+        l = label_comprassion(labels[idx])
+        print(f"{l:<{cell_width}}", end="")
+        for value in row:
+            print(f"{value:<{cell_width}}", end="")
+        print()
+    print(delimiter)
+
+capacity_matrix = build_dynamic_capachity_matrix(labels, edges)
+
+if args.matrix:
+    print_matrix_with_labels(capacity_matrix, labels)
+
+# Function for searching the increasing path (BFS)
+def bfs(capacity_matrix, flow_matrix, source, sink, parent):
+    visited = [False] * len(capacity_matrix)
+    queue = deque([source])
+    visited[source] = True
+
+    while queue:
+        current_node = queue.popleft()
+        
+        for neighbor in range(len(capacity_matrix)):
+            # Check if there is remaining bandwidth in the channel
+            if not visited[neighbor] and capacity_matrix[current_node][neighbor] - flow_matrix[current_node][neighbor] > 0:
+                parent[neighbor] = current_node
+                visited[neighbor] = True
+                if neighbor == sink:
+                    return True
+                queue.append(neighbor)
+    
+    return False
+
+# Basic function to calculate maximum flow
+def edmonds_karp(capacity_matrix, source, sink):
+    num_nodes = len(capacity_matrix)
+    flow_matrix = [[0] * num_nodes for _ in range(num_nodes)]
+    parent = [-1] * num_nodes
+    max_flow = 0
+
+    # As long as there is an increasing path, add a stream
+    while bfs(capacity_matrix, flow_matrix, source, sink, parent):
+        # Find the minimum throughput along the found path (bottleneck)
+        path_flow = float('Inf')
+        current_node = sink
+
+        while current_node != source:
+            previous_node = parent[current_node]
+            path_flow = min(path_flow, capacity_matrix[previous_node][current_node] - flow_matrix[previous_node][current_node])
+            current_node = previous_node
+        
+        # Update the flow along the path, taking into account the return flow
+        current_node = sink
+        while current_node != source:
+            previous_node = parent[current_node]
+            flow_matrix[previous_node][current_node] += path_flow
+            flow_matrix[current_node][previous_node] -= path_flow
+            current_node = previous_node
+        
+        # Increase the maximum flow
+        max_flow += path_flow
+
+    return max_flow
+
+# Print the maximum flow from each source to each sink
+for i_idx, source in enumerate(sources):
+    print(f"From '{source}'")
+    for j_idx, sink in enumerate(sinks):
+        print(f"From '{source}' to '{sink}': {edmonds_karp(capacity_matrix, labels.index(source), labels.index(sink))}")
